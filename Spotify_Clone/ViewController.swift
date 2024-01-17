090d79376377282c46c19d1b09d07b9650f93a54
BearyCode <rettingerm@protonmail.com>
2024-01-17 17:20:37 +0100

    Initial Commit

---
+++ Spotify_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify_Clone
+//
+//  Created by BearyCode on 17.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
